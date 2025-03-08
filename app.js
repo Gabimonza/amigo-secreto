5796f2031db6b8a2b4c92a128f7c249fed6dd01f
Gabriel Monza <gabimonza2@gmail.com>
2025-03-08 06:05:18 -0300

    agregando funcion agregarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+function agregarAmigo() {
+    const input = document.getElementById("amigo");
+    const nombre = input.value.trim();
+
+    if (nombre === "" ){
+        alert("Ingrese un nombre");
+        console.log("no se ingreso nombre");
+        return;
+    }
+    
+    amigos.push(nombre);
+    input.value= "" ;
+
+    let contador = amigos.length - 1; 
+    console.log(`Amigo agregado:${amigos[contador]}`); 
+
+}
+
